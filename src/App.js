2501222dde0a18c24ad01de81b2e3cc929cfd795
Sagar1917118 <112215153@cse.iiitp.ac.in>
2024-07-07 13:14:39 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import CompBody from './Components/CompBody';
+import Cart from './Components/Cart';
+import {Routes,Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col  justify-center items-center gap-2">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<CompBody/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+     </Routes>
     </div>
   );
 }
